d4eeed1eccc657862557ccf19f82e8580eb5c8cb
TheoryDance <1378926190@qq.com>
2019-08-01 13:46:51 +0800

    将注释掉的包引入，去掉

--- src/main/java/com/theorydance/myspringboot/Application.java
+++ src/main/java/com/theorydance/myspringboot/Application.java
@@ -2,14 +2,16 @@ package com.theorydance.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
-		ApplicationContext application = SpringApplication.run(Application.class, args);
-		/*LogMaster handler = application.getBean(LogMaster.class);
+		
+		SpringApplication.run(Application.class, args);
+		
+		/*ApplicationContext application = SpringApplication.run(Application.class, args);
+		LogMaster handler = application.getBean(LogMaster.class);
 		try {
 			handler.uploadLogsToES();
 		} catch (Exception e) {
