b16a27126f59569a8b84272cb932b97c87925d7c
TheoryDance <1378926190@qq.com>
2019-08-01 13:46:01 +0800

    使用推荐方式编写代码

--- src/main/java/com/theorydance/myspringboot/comm/WebConfiguration.java
+++ src/main/java/com/theorydance/myspringboot/comm/WebConfiguration.java
@@ -5,6 +5,8 @@ import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration {
@@ -29,6 +31,17 @@ public class WebConfiguration {
 		// order的值设置的越低，越优先执行
 		registration.setOrder(2);
 		return registration;
+	}
+	
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/lucene/index").setViewName("lucene/index");
+				registry.addViewController("/lucene/detail").setViewName("lucene/detail");
+			}
+		};
 	}
 	
 }
